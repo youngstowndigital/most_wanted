8c5fa628e6cd69373049e1c5e24bfe99a2aab167
Mike Wittenauer <mikewittenauer@me.com>
2022-01-02 12:15:30 -0500

    Add moment and 'last updated' data

--- src/containers/WantedPage.js
+++ src/containers/WantedPage.js
@@ -29,6 +29,7 @@ const WantedPage = () => {
                     <li className="list-group-item"><strong>Sex:</strong> { selectedWanted.sex ?? 'Unknown' }</li>
                     <li className="list-group-item"><strong>Age (min-max):</strong> { selectedWanted.age_min ?? 'Unknown' } - { selectedWanted.age_max ?? 'Unknown'  }</li>
                     <li className="list-group-item"><strong>Race:</strong> { selectedWanted.race_raw ?? 'Unknown'  }</li>
+                    <li className="list-group-item"><strong>Weight (min-max):</strong> { selectedWanted.weight_min ?? 'Unknown' } - { selectedWanted.weight_max ?? 'Unknown'  }</li>
                 </ul>
             </div>
             {
@@ -36,6 +37,8 @@ const WantedPage = () => {
                 <div className="col-md-8">
                     <h2>Caution:</h2>
                     { selectedWanted.caution && <div dangerouslySetInnerHTML={{ __html: selectedWanted.caution.replace(/\s+/g, ' ').trim() }}></div> }
+                    <h2>Description:</h2>
+                    { selectedWanted.description && <div dangerouslySetInnerHTML={{ __html: selectedWanted.description.replace(/\s+/g, ' ').trim() }}></div> }
                     <h2>Remarks:</h2>
                     { selectedWanted.remarks && <div dangerouslySetInnerHTML={{ __html: selectedWanted.remarks.replace(/\s+/g, ' ').trim() }}></div> }
                 </div>
@@ -43,6 +46,8 @@ const WantedPage = () => {
                 <div className="col-md-8">
                     <h2>Details:</h2>
                     { selectedWanted.details && <div dangerouslySetInnerHTML={{ __html: selectedWanted.details.replace(/\s+/g, ' ').trim() }}></div> }
+                    <h2>Description:</h2>
+                    { selectedWanted.description && <div dangerouslySetInnerHTML={{ __html: selectedWanted.description.replace(/\s+/g, ' ').trim() }}></div> }
                 </div>
             }
         </div>
