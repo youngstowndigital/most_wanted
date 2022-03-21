e1c0e14c7dda01f9177bfa3cf3e6ec8cb24d0715
Mike Wittenauer <mikewittenauer@Mikes-MacBook-Pro.local>
2022-03-20 20:17:03 -0400

    Update favicon and logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import fbi_logo from '../fbi_logo.png'
 
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Link to="/" className="navbar-brand">FBI Most Wanted</Link>
+            <div className="container">
+                <Link to="/" className="navbar-brand">
+                    <img src={fbi_logo} className="logo mx-3" />
+                    FBI Most Wanted
+                </Link>
             </div>
         </nav>
     )
